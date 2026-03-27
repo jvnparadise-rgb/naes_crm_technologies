1c51af1fff80e37cbcf0a6eccb0a02ffad143f84
Jeff Yarbrough <jvnparadise@gmail.com>
2026-03-27 09:36:25 -0700

    checkpoint: add account and contact domain contracts as opportunity dependencies

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,2 +1,5 @@
 export { opportunityDomainContract } from './opportunityDomainContract.js';
 export { validateOpportunityDomainContract } from './validateOpportunityDomainContract.js';
+export { accountDomainContract } from './accountDomainContract.js';
+export { contactDomainContract } from './contactDomainContract.js';
+export { validateRelatedDomainContracts } from './validateRelatedDomainContracts.js';
